196d657a25d59a936849c188b7988600481b6f20
Khrystynasrd <kserednytska@gmail.com>
2024-03-06 17:47:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        int c = 35;
+        System.out.println(c/10+c%10);
+
+        double d = 3.5;
+        System.out.println((int)(d+0.5));
+    }
+}
